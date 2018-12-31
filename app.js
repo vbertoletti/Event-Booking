3000047bf0cb702e86fab304e9f1ea08781702d2
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-12-31 15:48:17 -0800

    add Event type on schema and its input args

--- app.js
+++ app.js
@@ -5,19 +5,37 @@ const { buildSchema } = require("graphql");
 
 const app = express();
 
+//TODO: remove in memory storing data
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+
+      }
 
       type RootQuery {
-        events: [String!]!
+        events: [Event!]!
       }
 
       type RootMutation {
-        createEvent(name: String): String
+        createEvent(eventInput: EventInput): Event
       }
       schema {
           query: RootQuery
@@ -27,13 +45,20 @@ app.use(
     //resolvers
     rootValue: {
       events: () => {
-        return ["Event 1", "Event 2", "Event 3"];
+        return events;
       },
 
-      //this will take in the args passed when creating the schema. Example, takes in name.
+      //this will take in the args passed when creating the schema.
       createEvent: args => {
-        const eventName = args.name;
-        return eventName;
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
       }
     },
     //graphql playground
