5a065c2c9fb87bce54ac119b1569cc0e09c7e830
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2018-12-31 16:52:06 -0800

    remove in-memory storage and connect MongoDB

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
+const Event = require("./models/event.js");
 
 const app = express();
-
-//TODO: remove in memory storing data
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -45,20 +44,38 @@ app.use(
     //resolvers
     rootValue: {
       events: () => {
-        return events;
+        return Event.find()
+          .then(events => {
+            return events.map(event => {
+              //I need to transform the _id into a string to solve graphql error. Mongoose allows me to use only event.id - this also works fine.
+              return { ...event._doc, _id: event._doc._id.toString() };
+            });
+          })
+          .catch(err => {
+            throw err;
+          });
       },
 
       //this will take in the args passed when creating the schema.
       createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
+        const event = new Event({
           title: args.eventInput.title,
           description: args.eventInput.description,
           price: +args.eventInput.price,
-          date: args.eventInput.date
-        };
-        events.push(event);
-        return event;
+          date: new Date(args.eventInput.date)
+        });
+        //save method is provided by mongoose
+        return event
+          .save()
+          .then(result => {
+            console.log(result);
+            //_doc is provided by mongoose, it leaves out meta data
+            return { ...result._doc, _id: result._doc._id.toString() };
+          })
+          .catch(err => {
+            console.log(err);
+            throw err;
+          });
       }
     },
     //graphql playground
@@ -66,4 +83,15 @@ app.use(
   })
 );
 
-app.listen(3000);
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${
+      process.env.MONGO_PASSWORD
+    }@cluster0-uagfb.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`
+  )
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
