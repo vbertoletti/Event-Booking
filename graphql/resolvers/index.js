d74bc8d74cfcad8403acf691d52d5dd840b54099
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2019-01-02 20:59:29 -0800

    add booking model

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -1,5 +1,6 @@
 const Event = require("../../models/event.js");
 const User = require("../../models/user.js");
+const Booking = require("../../models/booking.js");
 const bcrypt = require("bcryptjs");
 
 const events = async eventIds => {
@@ -14,6 +15,19 @@ const events = async eventIds => {
       };
     });
     return events;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const singleEvent = async eventId => {
+  try {
+    const event = await Event.findById(eventId);
+    return {
+      ...event._doc,
+      _id: event.id,
+      creator: user.bind(this, event.creator)
+    };
   } catch (err) {
     throw err;
   }
@@ -44,6 +58,24 @@ module.exports = {
           _id: event._doc._id.toString(),
           date: new Date(event._doc.date).toISOString(),
           creator: user.bind(this, event._doc.creator)
+        };
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+
+  bookings: async () => {
+    try {
+      const bookings = await Booking.find();
+      return bookings.map(booking => {
+        return {
+          ...booking._doc,
+          _id: booking.id,
+          createdAt: new Date(booking._doc.createdAt).toISOString(),
+          updatedAt: new Date(booking._doc.updatedAt).toISOString(),
+          user: user.bind(this, booking._doc.user),
+          event: singleEvent.bind(this, booking._doc.event)
         };
       });
     } catch (err) {
@@ -110,5 +142,21 @@ module.exports = {
     } catch (err) {
       throw err;
     }
+  },
+  bookEvent: async args => {
+    const fetchedEvent = await Event.findOne({ _id: args.eventId });
+    const booking = new Booking({
+      user: "5c2c0a11d91ce34987f3de40",
+      event: fetchedEvent
+    });
+    const result = await booking.save();
+    return {
+      ...result._doc,
+      _id: result.id,
+      createdAt: new Date(result._doc.createdAt).toISOString(),
+      updatedAt: new Date(result._doc.updatedAt).toISOString(),
+      user: user.bind(this, booking._doc.user),
+      event: singleEvent.bind(this, booking._doc.event)
+    };
   }
 };
---
+++ models/booking.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema(
+  {
+    event: {
+      type: Schema.Types.ObjectId,
+      ref: "Event"
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+  },
+  //so mongoose will provide a created/updated timestamp
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Booking", bookingSchema);
