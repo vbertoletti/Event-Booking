dbabdf3008daa90c2325d8436f57b67e06be225f
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2019-01-02 21:14:22 -0800

    add bookevent and cancelbooking resolvers, db gets updated accordingly

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -158,5 +158,19 @@ module.exports = {
       user: user.bind(this, booking._doc.user),
       event: singleEvent.bind(this, booking._doc.event)
     };
+  },
+  cancelBooking: async args => {
+    try {
+      const booking = await Booking.findById(args.bookingId).populate("event");
+      const event = {
+        ...booking.event._doc,
+        _id: booking.event.id,
+        creator: user.bind(this, booking.event._doc.creator)
+      };
+      await Booking.deleteOne({ _id: args.bookingId });
+      return event;
+    } catch (err) {
+      throw err;
+    }
   }
 };
