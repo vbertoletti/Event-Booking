915198d59f0fbbb2ae3364385821cc70b69ed6b8
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2019-01-17 11:31:05 -0800

    add backdrop component to shade the background of my modal

---
+++ client/src/components/Backdrop/Backdrop.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Backdrop.css";
+
+const Backdrop = props => {
+  return <div className="backdrop" />;
+};
+
+export default Backdrop;
--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import "./Events.css";
 import Modal from "../components/Modal/Modal";
+import Backdrop from "../components/Backdrop/Backdrop";
 
 class EventsPage extends Component {
   render() {
     return (
       <React.Fragment>
+        <Backdrop />
         <Modal title="Add Event" canCancel canConfirm>
           <p>Modal Content</p>
         </Modal>
