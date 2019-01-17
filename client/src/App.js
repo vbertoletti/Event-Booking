430c150e5ef58e864b3445652b8065f1edbb12fb
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2019-01-17 10:47:12 -0800

    adjust some routes for the user is logged in and logged out and adjust simple styles on the nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -36,7 +36,6 @@ class App extends Component {
             <MainNavigation />
             <main className="main-content">
               <Switch>
-                {!this.state.token && <Redirect from="/" to="/auth" exact />}
                 {this.state.token && <Redirect from="/" to="/events" exact />}
                 {this.state.token && (
                   <Redirect from="/auth" to="/events" exact />
@@ -48,6 +47,7 @@ class App extends Component {
                 {this.state.token && (
                   <Route path="/bookings" component={BookingsPage} />
                 )}
+                {!this.state.token && <Redirect to="/auth" exact />}
               </Switch>
             </main>
           </AuthContext.Provider>
