5dc45dc0379d41ca23755f2b993b31485139b038
Vanessa Bertoletti <vanessabertoletti@gmail.com>
2019-01-17 10:55:05 -0800

    add a bit of content to the events page so user can create their own events

--- client/src/pages/Events.js
+++ client/src/pages/Events.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import "./Events.css";
 
 class EventsPage extends Component {
   render() {
-    return <h1>the events page</h1>;
+    return (
+      <div className="events-control">
+        <p>Share your own events!</p>
+        <button className="btn">Create Event</button>
+      </div>
+    );
   }
 }
 
